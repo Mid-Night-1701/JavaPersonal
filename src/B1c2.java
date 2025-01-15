c756e5aaa031f87035156f63d7576cd3e9947a2d
Arisu171 <long171.d.shine@gmail.com>
2025-01-15 14:53:59 +0700

    bruh

--- src/B1c2.java
+++ src/B1c2.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 
 public class B1c2 {
     public static void main(String[] args) {
-        Scanner scn = new Scanner(System.in);
-        System.out.print("Scan a, b, c, d: ");
-        int[] A = new int[4];
-        for (int i = 0; i < 4; i++) {
-            A[i] = scn.nextInt();
+        try (Scanner scn = new Scanner(System.in)) {
+            System.out.print("Scan a, b, c, d: ");
+            int[] A = new int[4];
+            for (int i = 0; i < 4; i++) {
+                A[i] = scn.nextInt();
+            }
+            Arrays.sort(A);
+            System.out.println("Max1: " + A[3]);
+            System.out.println("Max2: " + A[2]);
         }
-        Arrays.sort(A);
-        System.out.println("Max1: " + A[3]);
-        System.out.println("Max2: " + A[2]);
-        scn.close();
     }
 }
 
